8be01652029eb6c122ac173617e12006446294c1
Uzwal-og <153886946+Uzwal-og@users.noreply.github.com>
2023-12-15 06:21:17 +0000

    test deleted

---
+++ 01_basics/Basics.js
@@ -0,0 +1,26 @@
+/*
+----CONSOLE LOG AND VER,LET AND CONST----
+
+console.log("Hello World");
+const Chaddi = 9191;
+Underwear="Rupasa, Macho, '992920'"
+
+Chaddi="Lingeri"
+console.table([Chaddi,Underwear])
+
+*/
+
+
+
+//----Data Types----//
+// Number
+// bigint
+// String
+// Boolean=> True/False
+// null= standalone value
+// undefined
+// symbol
+
+
+console.log(typeof "Hello")
+
